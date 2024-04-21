9da30ea276bf2a81961d25a45aaad4bf04b05d86
MinnMinn <adramblink@gmail.com>
2024-04-21 23:13:01 +0630

    Made Changes

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use('/images', express.static(path.join(__dirname, 'upload', 'images')));
 app.post("/upload", upload.single("product"), (req, res) => {
     res.json({
         success: 1,
-        image_url: `http://${req.hostname}:${port}/images/${req.file.filename}` // Dynamically generate image URL
+        image_url: `/images/${req.file.filename}` // Dynamically generate image URL
     });
 });
 
