3dbed7123a9e1d68d7413643a84ffc6baa72b88a
MinnMinn <adramblink@gmail.com>
2024-04-22 22:23:36 +0630

    Made Changes

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use('/images', express.static(path.join(__dirname, 'upload', 'images')));
 app.post("/upload", upload.single("product"), (req, res) => {
     res.json({
         success: 1,
-        image_url: `/images/${req.file.filename}` // Dynamically generate image URL
+        image_url: `https://vicherserver.onrender.com/${req.file.filename}` // Dynamically generate image URL
     });
 });
 
